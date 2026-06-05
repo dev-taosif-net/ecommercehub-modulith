a6f00fb6cd6f8a70523772b3da8b397cca18f471
dev-taosif-net <dev.taosif.net@gmail.com>
2026-06-05 21:59:23 +0600

    Add custom exception handler for improved error management

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,13 +1,19 @@
+using Shared.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddModules(builder.Configuration)
     .AddApiSwagger();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 app.UseModules();
 app.UseApiSwagger();
+
+app.UseExceptionHandler(options => { });
 
 app.MapGet("/api/test", () => "Api is working.");
 
